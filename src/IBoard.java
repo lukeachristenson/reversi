5c33f0153b5fd554296d6edc668f4e80bc863c7a
lukechristenson <christenson.lu@northeastern.edu>
2023-10-26 17:55:11 -0400

    everything except validMove

--- src/IBoard.java
+++ src/IBoard.java
@@ -2,16 +2,16 @@ import java.util.Optional;
 
 public interface IBoard {
   /**
-   * Adds a cell to the board
-   * @param cell
-   * @param player
+   * Adds a cell to the board.
+   * @param cell the cell to be added.
+   * @param player the player to be added.
    */
   public void newCellOwner(ICell cell, Optional<Player> player);
 
   /**
    * Gets the state of the cell, meaning who owns it currently, or if it is empty.
    *
-   * @param cell
+   * @param cell the cell to get the state of.
    */
   public Optional<Player> getCellState(ICell cell);
 
