433278c4df512621ab7885fafbc63a98ab5a9b59
akala47 <sunnyshrikantrk@gmail.com>
2023-10-27 12:52:50 -0400

    Added a textView that can be used for testing.

--- src/view/ReversiTextView.java
+++ src/view/ReversiTextView.java
@@ -1,12 +1,13 @@
 package view;
 
 import model.HexagonReversi;
+import model.IReversiModel;
 
 public class ReversiTextView implements TextView{
-  private final HexagonReversi model;
+  private final IReversiModel model;
   private Appendable appendable;
 
-  public ReversiTextView(HexagonReversi model) {
+  public ReversiTextView(IReversiModel model) {
     this.model = model;
   }
 
--- test/ExampleBoardTests.java
+++ test/ExampleBoardTests.java
@@ -8,14 +8,18 @@ import java.util.Optional;
 
 import model.HexagonBoard;
 import model.HexagonCell;
+import model.HexagonReversi;
+import model.IReversiModel;
 import model.Player;
+import view.ReversiTextView;
+import view.TextView;
 
 
 public class ExampleBoardTests {
 
   @Test
   public void testHexagonBoardToString() {
-    HexagonBoard hexagonBoard = new HexagonBoard(3);
+    HexagonBoard hexagonBoard = new HexagonBoard(2);
     // Populate the board with some cells and players
     hexagonBoard.newCellOwner(new HexagonCell(0, 0, 0), Optional.of(Player.BLACK));
     hexagonBoard.newCellOwner(new HexagonCell(1, -1, 0), Optional.of(Player.WHITE));
@@ -28,5 +32,4 @@ public class ExampleBoardTests {
 
     System.out.println(hexagonBoard.toString());
   }
-
 }
