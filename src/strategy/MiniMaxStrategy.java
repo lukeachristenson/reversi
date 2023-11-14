182f18ebe5fb0c57385d90604b3d19266157e83c
akala47 <sunnyshrikantrk@gmail.com>
2023-11-13 21:04:04 -0500

    broken MiniMaxStrategy

--- src/strategy/MiniMaxStrategy.java
+++ src/strategy/MiniMaxStrategy.java
@@ -2,7 +2,6 @@ package strategy;
 
 import model.Color;
 import model.HexagonReversi;
-import model.HumanPlayer;
 import model.IBoard;
 import model.ICell;
 import model.IReversiModel;
@@ -14,11 +13,9 @@ import java.util.List;
 
 public class MiniMaxStrategy implements Strategy {
   private final Color color;
-  private final int depth;
 
-  public MiniMaxStrategy(Color color, int depth) {
+  public MiniMaxStrategy(Color color) {
     this.color = color;
-    this.depth = depth;
   }
 
   @Override
@@ -28,69 +25,67 @@ public class MiniMaxStrategy implements Strategy {
 
 
 
+    for (ICell cell : model.getValidMoves(color)) {
+      // Create model copy with the board.
+      IReversiModel newModel = new HexagonReversi(model.createBoardCopy(), model.getDimensions());
+
+      // If for any cell in the list of valid moves left, the game is over, add the weight of the cell accordingly.
+
+      // Else, evaluate the opponent's best move and add the weight of the cell accordingly.
+
+
+      int weight = model.cellsFlipped(cell, color);
+      IBoard newBoard = model.createBoardCopy();
+      newBoard.validMove(cell, color, true);
+      IReversiModel newModel = new HexagonReversi(newBoard, model.getDimensions());
+      moveScores.put(cell, weight + evaluateOpponentMax(newModel));
+    }
+
     return null;
   }
 
-  private int evaluateMaxScore(ROModel model, int reducedDepth, int originalDepth, Color color) {
-    int maxScore = 0;
-    int weightFactor;
-    HashMap<ICell, Integer> moveScores = new HashMap<>();
-    List<ICell> moves  = model.getValidMoves(color);
+  private int evaluateOpponentMax(ROModel model) {
+    List<ICell> validMoves = model.getValidMoves(getOtherColor(color));
+    int max = Integer.MIN_VALUE;
 
-    // Set the weight factor to -1 if color is opposite to the strategy's color. Else, set it to 1.
-    if((originalDepth - reducedDepth)%2 == 0){
-      weightFactor = 1;
-    } else {
-      weightFactor = -1;
-    }
-
-    // If the depth is 1, return the number of cells flipped by the move.
-    if(reducedDepth == 1) {
-      for(ICell cell : moves) {
-        if(model.cellsFlipped(cell, color) >= maxScore) {
-          maxScore = model.cellsFlipped(cell, color);
-        }
-      }
-      return maxScore * weightFactor;
-    } else {
-      // For each ICell in moves, map it to the maximum score of the next depth.
-      for(ICell cell : moves) {
-        IReversiModel modelCopy = new HexagonReversi(model.createBoardCopy(), model.getDimensions());
-        modelCopy.placePiece(cell, new HumanPlayer(color));
-        moveScores.put(cell,evaluateMaxScore(modelCopy, reducedDepth-1, originalDepth, getOtherColor(color)));
-      }
-      for(ICell cell : moveScores.keySet()) {
-
-
+    if (model.isGameOver()) {
+      // If
+      if (model.getScore(color) < model.getScore(getOtherColor(color))) {
+        return Integer.MIN_VALUE;
+      } else {
+        return Integer.MAX_VALUE;
       }
     }
 
-
-    IBoard board = model.createBoardCopy();
-
-
-    for(ICell move : moves) {
-      int score = evaluateMaxScore(new HexagonReversi(board, model.getDimensions()), moves, reducedDepth - 1, originalDepth, getOtherColor());
-      if(score > maxScore) {
-        maxScore = score;
+    for (ICell cell : validMoves) {
+      int score = model.cellsFlipped(cell, color);
+      if (score > max) {
+        max = score;
       }
     }
 
-    if(reducedDepth == 1) {
-
-    }
-
-    return 0;
+    return -max;
   }
 
+
+  private int returnGameOverWeight(ROModel model) {
+    if (model.isGameOver()) {
+      // If
+      if (model.getScore(color) < model.getScore(this.getOtherColor(color))) {
+        return Integer.MIN_VALUE;
+      } else {
+        return Integer.MAX_VALUE;
+      }
+    }
+    return 0;
+  }
 
 
   private Color getOtherColor(Color color) {
     if (color.equals(Color.BLACK)) {
       return Color.WHITE;
-    }
-    return Color.BLACK;
+    } else
+      return Color.BLACK;
   }
-
-
 }
+
