7593001b0874e826add83b500ee4f01f58d5fdd2
akala47 <sunnyshrikantrk@gmail.com>
2023-12-06 21:37:09 -0500

    view works, strategy doesn't

--- src/cs3500/reversi/TestingReversiRunner.java
+++ src/cs3500/reversi/TestingReversiRunner.java
@@ -7,7 +7,6 @@ import cs3500.provider.controller.ControllerFeatures;
 import cs3500.provider.model.ModelAdapter;
 import cs3500.provider.model.ReversiModel;
 import cs3500.provider.view.GUI;
-import cs3500.provider.view.ViewInterface;
 import cs3500.reversi.model.HexagonReversi;
 import cs3500.reversi.model.IReversiModel;
 import cs3500.reversi.model.TokenColor;
@@ -24,34 +23,32 @@ public final class TestingReversiRunner {
    */
   public static void main(String[] args) {
 
-      IReversiModel ourModel = new HexagonReversi(6);
+    IReversiModel ourModel = new HexagonReversi(6);
 
-      // Create the first view and controller
-      ReversiModel theirModel1 = new ModelAdapter(ourModel);
-      ViewInterface view1 = new GUI(theirModel1);
-      ControllerFeatures controller1 = new ControllerAdapter(theirModel1, TokenColor.BLACK, view1);
-      theirModel1.startGame(6, 2);
+    // Create the first view and controller
+    ReversiModel theirModel1 = new ModelAdapter(ourModel);
+    GUI view1 = new GUI(theirModel1);
+    ControllerFeatures controller1 = new ControllerAdapter(theirModel1, TokenColor.BLACK, view1);
 
-      JFrame frame1 = new JFrame("Reversi Game - Player 1");
-      frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame1.getContentPane().add((JPanel) view1);
-      frame1.pack();
-      frame1.setSize(1000, 1000);
-      frame1.setLocationRelativeTo(null);
-      frame1.setVisible(true);
+    JFrame frame1 = new JFrame("Reversi Game - Player 1");
+    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame1.add(view1);
+    frame1.pack();
+    frame1.setSize(500, 500);
+    frame1.setLocationRelativeTo(null);
+    frame1.setVisible(true);
 
-      // Create the second view and controller
-      ViewInterface view2 = new GUI(theirModel1);
-      ControllerFeatures controller2 = new ControllerAdapter(theirModel1, TokenColor.WHITE, view2);
-      theirModel1.startGame(6, 2);
+    // Create the second view and controller
+    GUI view2 = new GUI(theirModel1);
+    ControllerFeatures controller2 = new ControllerAdapter(theirModel1, TokenColor.WHITE, view2);
+    theirModel1.startGame(6, 2);
 
-      JFrame frame2 = new JFrame("Reversi Game - Player 2");
-      frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame2.getContentPane().add((JPanel) view2);
-      frame2.pack();
-      frame2.setSize(1000, 1000);
-      frame2.setLocationRelativeTo(null);
-      frame2.setVisible(true);
+    JFrame frame2 = new JFrame("Reversi Game - Player 2");
+    frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame2.add(view2);
+    frame2.setSize(500, 500);
+    frame2.setVisible(true);
+    theirModel1.startGame(6, 2);
 
   }
 }
